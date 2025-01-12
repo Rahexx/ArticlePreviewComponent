6edf511114ddcd42fabdb924a8e241122134aa1a
Andrzej Łuszcz <endriu1297@gmail.com>
2025-01-12 17:10:54 +0100

    feat: add animation for showing and hiding share panel

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const share = document.querySelector('.share');
 const shareBtn = share.querySelector('.share__btn');
 console.log(shareBtn);
 articleBtn.addEventListener('click', () => {
-  share.classList.toggle('share--show');
+  share.classList.remove('share--hide');
+  share.classList.add('share--show');
 });
 
 shareBtn.addEventListener('click', () => {
-  share.classList.toggle('share--show');
+  share.classList.remove('share--show');
+  share.classList.add('share--hide');
 });
