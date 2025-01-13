60e3e42127dfb3e241148b1cfdc0cccab2529cfb
Andrzej Łuszcz <endriu1297@gmail.com>
2025-01-14 00:34:59 +0100

    feat: resolve issue with hiding share component on app start

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const articleBtn = document.querySelector('.article__btn');
 const share = document.querySelector('.share');
 const shareBtn = share.querySelector('.share__btn');
-console.log(shareBtn);
+
 articleBtn.addEventListener('click', () => {
-  if (share.classList.contains('share--hide')) {
-    share.classList.remove('share--hide');
+  if (
+    share.classList.contains('share--initial') ||
+    share.classList.contains('share--hide')
+  ) {
+    share.classList.remove('share--initial', 'share--hide');
     share.classList.add('share--show');
   } else {
-    share.classList.add('share--hide');
     share.classList.remove('share--show');
+    share.classList.add('share--hide');
   }
 });
 
